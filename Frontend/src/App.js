1bed7f45e79373a3084f9b70c6f39de9279a1509
Paniv45 <kapoorpaniv45@gmail.com>
2025-02-26 19:12:33 +0530

    updated ui

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,30 +1,35 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-// import ChatGallery from "./components/ChatGallery";
-// import ChatRoom from "./components/ChatRoom";
 import ChatLayout from "./components/ChatLayout";
+import ChatNavbar from "./components/ChatNavbar";
 
 function App() {
-  
   return (
     <Router>
-      
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            {/* <Route path="/chat-gallery" element={<ChatGallery />} />
-            <Route path="/chat/:roomID" element={<ChatRoom />} /> */}
-             <Route path="/chat-gallery" element={<ChatLayout />} />
-             <Route path="/chat/:roomID" element={<ChatLayout />} />
-
-          </Routes>
-      
-      
+      <MainContent />
     </Router>
   );
 }
 
-export default App;
+function MainContent() {
+  const location = useLocation();
 
+  // ChatNavbar sirf chat pages par dikhega
+  const showNavbar = location.pathname.startsWith("/chat");
+
+  return (
+    <>
+      {showNavbar && <ChatNavbar />}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/chat-gallery" element={<ChatLayout />} />
+        <Route path="/chat/:roomID" element={<ChatLayout />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend/src/components/ChatNavbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/ChatNavbar.css";
+
+function ChatNavbar() {
+
+  return (
+    <nav className="chat-navbar">
+      <h2>Baat Cheet</h2>
+    </nav>
+  );
+}
+
+export default ChatNavbar;
